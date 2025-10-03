adbfe83616ba6d552defccf3a5c77d9458a3ed8a
Juha Tauriainen <juha@bin.fi>
2025-10-03 15:39:15 +0300

    Add state to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,18 @@
+import {useState} from 'react';
+
 const Button = () => {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState('juha');
+
   return (
     <button
       onClick={() => {
-        console.log('Button was clicked');
+        setCounter(counter + 1);
+        console.log('counter', counter);
+        setName(`${name}-${counter}`);
       }}
     >
-      click here
+      click here ({counter}) ({name})
     </button>
   );
 };
